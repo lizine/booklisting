90db96bc33c8fcaa2801d1d530269aaef7910d60
Nina <nina.liljeblad@gmail.com>
2016-03-27 16:39:02 +0300

    adding booklist and bookdetail containers

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">
+        <li
+          key={book.title}
+          onClick={()=> this.props.selectBook(book)}
+          className="list-group-item">
           {book.title}
         </li>
       );
@@ -25,4 +30,11 @@ function mapStateToProps(state){
     books: state.books
   };
 }
-export default connect(mapStateToProps)(BookList);
+//anythin returned from this function will be props to BookList container
+function mapDispatchToProps(dispatch){
+  /* whenever selectBook is called, result should be passed
+  to all of our reducers */
+  return bindActionCreators({selectBook : selectBook}, dispatch)
+}
+//Promote BookList from component to container needs to know aobout this new method
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
